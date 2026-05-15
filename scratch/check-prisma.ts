b036e51868082365d282c112955e877d39f66fea
Nazilah1998 <nazilahmuhammad1998@gmail.com>
2026-05-16 02:36:32 +0700

    chore: remove scratch file and finalize all production fixes

--- scratch/check-prisma.ts
+++
@@ -1,7 +0,0 @@
-import prisma from "./lib/prisma";
-
-async function main() {
-  console.log("Models in prisma:", Object.keys(prisma).filter(k => !k.startsWith("$") && !k.startsWith("_")));
-}
-
-main();
